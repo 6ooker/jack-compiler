54faf9220539df005dbaff71c1b58089e36d2953
6ooker <katzenberger.erik@gmail.com>
2023-11-10 09:35:17 +0100

    implemented required functionality

--- symbolTable.py
+++ symbolTable.py
@@ -16,17 +16,29 @@ class SymbolTable:
         self.subroutine_symbols.clear()
         self.index[STATIC] = self.index[VAR] = 0
     
-    def define(self, name, type, kind):
-        pass
+    def define(self, name, stype, kind):
+        self.symbols[kind][name] = (stype, kind, self.index[kind])
+        self.index[kind] += 1
     
     def varCount(self, kind) -> int:
-        pass
+        return sum(1 for n, (t, k, i) in self.symbols[kind].items() if k == kind)
     
     def kindOf(self, name):
-        pass
+        (stype, kind, index) = self.lookup(name)
+        return kind
     
     def typeOf(self, name) -> str:
-        pass
+        (stype, kind, index) = self.lookup(name)
+        return stype
     
     def indexOf(self, name) -> int:
-        pass
+        (stype, kind, index) = self.lookup(name)
+        return index
+
+    def lookup(self, name) -> tuple:
+        if name in self.subroutine_symbols:
+            return self.subroutine_symbols[name]
+        elif name in self.class_symbols:
+            return self.class_symbols[name]
+        else:
+            return (None, None, None)
