e47661265f15b31e65c7fb19f2ebf7fd36ddc1b1
6ooker <katzenberger.erik@gmail.com>
2023-11-14 11:50:10 +0100

    adjusted main

--- jackAnalyzer.py
+++ jackAnalyzer.py
@@ -15,15 +15,13 @@ def main():
         if not os.path.basename(instream).endswith('.jack'):
             raise Exception("Must provide dir or single .jack file")
 
-        outfile = instream.replace('.jack', '.xml')
-        CE = CompilationEngine.CompilationEngine(instream, outfile)
+        CE = CompilationEngine.CompilationEngine(instream)
         CE.compileClass()
     
     elif os.path.isdir(instream):
         for jackFile in [f for f in os.listdir(instream) if f.endswith('.jack')]:
             infile = os.path.join(instream, jackFile)
-            outfile = infile.replace('.jack', '.xml')
-            CE = CompilationEngine.CompilationEngine(infile, outfile)
+            CE = CompilationEngine.CompilationEngine(infile)
             CE.compileClass()
 
 
